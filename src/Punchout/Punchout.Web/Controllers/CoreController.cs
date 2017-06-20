9a20ae97f8e6aee08ae1ed1ee19ae372734ee75d
Shawn Cooney <shawn.cooney@treasury.gov>
2017-06-20 11:05:12 -0400

    Created a hello world controller.

---
+++ src/Punchout/Punchout.Web/Controllers/CoreController.cs
@@ -0,0 +1,8 @@
+﻿namespace Punchout.Web.Controllers
+{
+    using System.Web.Mvc;
+
+    public class CoreController : Controller
+    {
+    }
+}
---
+++ src/Punchout/Punchout.Web/Controllers/HelloWorldController.cs
@@ -0,0 +1,21 @@
+﻿namespace Punchout.Web.Controllers
+{
+    using System.Web.Mvc;
+
+    public class HelloWorldController : CoreController
+    {
+        //
+        // GET: /HelloWorld/
+        public string Index()
+        {
+            return "This is my very first <b>controller!<b>";
+        }
+
+        //
+        // GET: /HelloWorld/Welcome/
+        public string Welcome()
+        {
+            return "<b>Welcome!<b>";
+        }
+    }
+}
